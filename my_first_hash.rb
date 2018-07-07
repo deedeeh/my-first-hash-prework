f806c8acd380cf8e391a653969f9484ca835c926
Dina Hafez <dinahafez.8@gmail.com>
2018-07-07 19:02:33 +0000

    automatically backed up by learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -1,6 +1,6 @@
 def my_hash
   # use the literal constructor to set the variable, my_hash, equal to a hash with key/value pairs of your choice.
-  your_hash = {"main_course" => "pasta", "desert" => "ice-cream"}
+  your_hash = {"main_course" => "pasta", "dessert" => "ice-cream"}
   
 end
 
